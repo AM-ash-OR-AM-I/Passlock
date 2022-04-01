d75c532bf135dc8a87cbd638783e18d03efa3b79
Ashutosh <ashutosh2gudu@gmail.com>
2022-04-01 18:23:39 +0530

    Cha]nged Backend

--- libs/Backend.py
+++ libs/Backend.py
@@ -1,34 +1,48 @@
-from typing import Dict
-
-
-def find_key(dictionary: Dict, text: str):
+def find_key(dictionary: dict, text: str):
 	weighted_pass = {}
 
-	def weight_passwords(text, name, password):
+	def weight_passwords_1(text: str, name: str, password: str):
+		def get_max_val(text: str, name: str, priority: float):
+			"""
+			Weights password based on length of searched text and actual no. of letters in the passwords.
+			"""
+			return max(len(text)/len(name), max_value) + priority
+
 		name_wo_space = name.replace(" ", "")
-		lower_text, lower_name = text.lower(), name.lower()
+		text_wo_space = text.replace(" ", "")
+		case_insensitive_text, case_insensitive_name = text.lower(), name.lower()
 		max_value = 0
-		priority = 0
 		if text == name_wo_space:
 			return True
 		else:
-			if text in name or lower_text in lower_name:
-				priority = 0
-				max_value = max(len(text) / len(name), len(lower_text) / len(lower_name), max_value) + priority
-			if text in name_wo_space:
-				priority = -0.01
-				max_value = max(len(text) / len(name), max_value) + priority
+			if text in name:  # Just checks if letteres searched are substring of the key in dictionary.
+				max_value = get_max_val(text, name, priority=0)
 
+			if case_insensitive_text in case_insensitive_name:  # Weights passwords without checking case.
+				max_value = get_max_val(text = case_insensitive_text, name=case_insensitive_name, priority=0)
+
+			if text in name_wo_space:  # Removes spaces from keys then checks whether text is found.
+				max_value = get_max_val(text, name=name_wo_space, priority= -0.01)
+
+			if text_wo_space in name_wo_space:  # Removes spaces both searched text and key and then weights password.
+				max_value = get_max_val(text=text_wo_space, name=name_wo_space, priority= -0.02)
+			
+			
 			if max_value>0:
 				weighted_pass[(name, password)] = max_value
 			return False
+	
+	def weight_passwords_2(text, name, password):
+		pass
 
-	def sort_keys(key_dict):
+	
+	def sort_keys(key_dict: dict):
 		key_arr = list(key_dict.items())
 		key_arr.sort(key=lambda x: x[1])  # Sorts the list based on the weighted values.
 		# weighted_pass = key_arr
 
 
+	
 	if text in dictionary or text.lower() in dictionary or text.upper() in dictionary:
 		return [(text, dictionary[text])]
 	else:
@@ -36,9 +50,11 @@ def find_key(dictionary: Dict, text: str):
 			"""
 			If text equals password without space then it returns that, else checks all passwords.
 			"""
-			if weight_passwords(text, name, password):
+			if weight_passwords_1(text, name, password):
 				return [(name, password)]
-
+		
+		if not weighted_pass:
+			weight_passwords_2(text, name, )
 	# sort_keys(weighted_pass)
 	return weighted_pass
 
