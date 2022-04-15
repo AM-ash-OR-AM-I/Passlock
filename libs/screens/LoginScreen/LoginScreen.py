e69f1e07ebdf03a525b7fccbf3bc611dc7b6249d
Ashutosh <ashutosh2gudu@gmail.com>
2022-04-15 21:05:08 +0530

    Updated files

--- libs/screens/LoginScreen/LoginScreen.py
+++ libs/screens/LoginScreen/LoginScreen.py
@@ -1,34 +1,28 @@
 from kivymd.app import MDApp
 from kivymd.toast import toast
 from kivymd.uix.screen import MDScreen
-from kivy.clock import Clock
 import threading
 
 app = MDApp.get_running_app()
 
-class LoginScreen(MDScreen):
 
-    def on_enter(self, *args):
-        Clock.schedule_once(lambda x:app.animate_login(self.ids.box),1.5)
-        def load_backend():
-            from libs.Backend import Encryption
-            self.encryption = Encryption
-        threading.Thread(target=load_backend).start()
+class LoginScreen(MDScreen):
 
     def login_button_pressed(self, email, password):
         def initialise_encryption():
+            from libs.Backend import Encryption
             try:
                 if app.fps: 
                     app.fps_monitor_start()
-                app.encryption_class = self.encryption(password)
+                app.encryption_class = Encryption(password)
                 app.passwords = app.encryption_class.load_decrypted()
                 encrypted_pass = app.encryption_class.load_passwords()
-                app.root.load_screen('HomeScreen')
-                app.root.HomeScreen.ids.create.ids.tab.switch_tab("[b]MANUAL")
                 for keys in encrypted_pass:
                     app.encrypted_keys[app.encryption_class.decrypt(keys)] = keys
             except UnicodeDecodeError:
                 toast('Invalid password')
         
+        app.root.load_screen('HomeScreen')
+        app.root.HomeScreen.ids.create.ids.tab.switch_tab("[b]MANUAL")
         threading.Thread(target=initialise_encryption, daemon=True).start()
         
