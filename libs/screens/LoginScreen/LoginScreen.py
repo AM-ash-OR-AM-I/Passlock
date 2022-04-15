29aaf375f8fe4ae82f020dc37420b2a508e081c3
Ashutosh <ashutosh2gudu@gmail.com>
2022-04-15 20:56:43 +0530

    Updated files

--- libs/screens/LoginScreen/LoginScreen.py
+++ libs/screens/LoginScreen/LoginScreen.py
@@ -5,24 +5,29 @@ import threading
 
 app = MDApp.get_running_app()
 
-
 class LoginScreen(MDScreen):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        def load_backend():
+            from libs.Backend import Encryption
+            self.encryption = Encryption
+        threading.Thread(target=load_backend).start()
 
     def login_button_pressed(self, email, password):
         def initialise_encryption():
-            from libs.Backend import Encryption
             try:
                 if app.fps: 
                     app.fps_monitor_start()
-                app.encryption_class = Encryption(password)
+                app.encryption_class = self.encryption(password)
                 app.passwords = app.encryption_class.load_decrypted()
                 encrypted_pass = app.encryption_class.load_passwords()
+                app.root.load_screen('HomeScreen')
+                app.root.HomeScreen.ids.create.ids.tab.switch_tab("[b]MANUAL")
                 for keys in encrypted_pass:
                     app.encrypted_keys[app.encryption_class.decrypt(keys)] = keys
             except UnicodeDecodeError:
                 toast('Invalid password')
         
-        app.root.load_screen('HomeScreen')
-        app.root.HomeScreen.ids.create.ids.tab.switch_tab("[b]MANUAL")
         threading.Thread(target=initialise_encryption, daemon=True).start()
         
