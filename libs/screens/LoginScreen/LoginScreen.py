9584e377c19fd80a592f450c1d0ff6a6069240c4
Ashutosh <ashutosh2gudu@gmail.com>
2022-04-15 20:44:44 +0530

    Updated files

--- libs/screens/LoginScreen/LoginScreen.py
+++ libs/screens/LoginScreen/LoginScreen.py
@@ -12,13 +12,13 @@ class LoginScreen(MDScreen):
         def initialise_encryption():
             from libs.Backend import Encryption
             try:
+                app.root.load_screen('HomeScreen')
+                app.root.HomeScreen.ids.create.ids.tab.switch_tab("[b]MANUAL")
                 if app.fps: 
                     app.fps_monitor_start()
                 app.encryption_class = Encryption(password)
                 app.passwords = app.encryption_class.load_decrypted()
                 encrypted_pass = app.encryption_class.load_passwords()
-                app.root.load_screen('HomeScreen')
-                app.root.HomeScreen.ids.create.ids.tab.switch_tab("[b]MANUAL")
                 for keys in encrypted_pass:
                     app.encrypted_keys[app.encryption_class.decrypt(keys)] = keys
             except UnicodeDecodeError:
